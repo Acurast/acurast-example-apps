ad35a1ccf7a5b87970eaaa0ff94292e7ad0bd416
Julia Samól <j.samol@papers.ch>
2025-04-09 16:06:51 +0200

    chore: update p2p relay address (#8)

--- apps/app-p2p/src/constants.ts
+++ apps/app-p2p/src/constants.ts
@@ -1,6 +1,5 @@
 export const PROTOCOL_MESSAGE_ECHO = "/message/echo/1.0.0";
 export const PROTOCOL_STREAM_ECHO = "/stream/echo/1.0.0";
 export const RELAYS = [
-  "/ip4/34.65.182.19/tcp/30093/ws/p2p/12D3KooWKGfsBKUArB33AyHJksf9aWLvy4PD5NTbwp1gpc1GDa2k",
-  "/ip4/34.65.160.177/tcp/30083/ws/p2p/12D3KooWSdrLcPjDBMyGfT5NehWaJvjSLEDKARuUS8eycQinRBzh",
+  "/ip4/95.216.144.79/tcp/9003/ws/p2p/12D3KooWD2AdgRQEtBEouYxuqNedG3zNqRqNJCo6KeFbHsxrrW8P",
 ];
